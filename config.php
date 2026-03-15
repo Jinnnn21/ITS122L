609d5b64c94ae51fccc6ef24ef020639cd0116c7
JohanTakkis <jtvvillafranca@mymail.mapua.edu.ph>
2026-03-15 21:34:11 +0800

    reverted congfig.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ session_start();
 
 try {
     $pdo = new PDO(
-        'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+        'mysql:host=' . DB_HOST . ';port=3307;dbname=' . DB_NAME . ';charset=utf8mb4',
         DB_USER,
         DB_PASS,
         [
